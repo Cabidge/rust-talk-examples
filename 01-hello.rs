9cd37b533bcb4de791c901af9ed28c636a3fcaa4
Cabidge <cabbageboi69@gmail.com>
2022-04-23 11:49:59 -0400

    +preface

--- 01-hello.rs
+++ 01-hello.rs
@@ -1,5 +1,9 @@
 // Double slashes denote comments
 
+// Rust is a general-purpose programming language
+// with a focus on high performance without
+// sacrificing memory safety
+
 // Rust has a C-like syntax with curly braces and semicolons
 // Functions are defined with the `fn` keyword
 // All rust programs start with a `main` function
