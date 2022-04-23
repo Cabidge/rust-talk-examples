967c5dd87862822ad12fad0c38adb1fb7c7cafef
Cabidge <cabbageboi69@gmail.com>
2022-04-23 12:09:36 -0400

    +more preface

--- 01-hello.rs
+++ 01-hello.rs
@@ -4,6 +4,13 @@
 // with a focus on high performance without
 // sacrificing memory safety
 
+// Rust is meant to be an alternative to languages like C and C++
+// while still feeling like a high level programming language
+
+// The following examples are meant to give a high-level overview
+// of the more interesting features of rust to help you decide
+// whether you'd like to choose rust as your next language to learn
+
 // Rust has a C-like syntax with curly braces and semicolons
 // Functions are defined with the `fn` keyword
 // All rust programs start with a `main` function
