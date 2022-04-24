1ff6822b7743aac22f0d68fa37f965c98f7008b0
Cabidge <cabbageboi69@gmail.com>
2022-04-24 16:01:41 -0400

    tweak

--- 09-iterator.rs
+++ 09-iterator.rs
@@ -1,33 +1,22 @@
 fn main() {
-    let arr = [1, 2, 3];
+    let v = vec![1, 2, 3];
 
-    // Create an iterator from an array
-    let mut iter = arr.iter();
-
-    // Iterators have a `next` function which returns an Option value
-    println!("{:?}", iter.next());
-    println!("{:?}", iter.next());
-    println!("{:?}", iter.next());
-    println!("{:?}", iter.next());
-
-    // Find the sum of all numbers in an iterator with the `sum` method
-    let result: i32 = arr.iter().sum();
-    println!("{}", result);
+    // Create an iterator from a vector
+    let iter = v.iter();
 
     // Loop over iterators with `for..in`
-    for i in arr.iter() {
+    for i in iter {
         println!("{}", i);
     }
+
+    // Find the sum of all numbers in an iterator with the `sum` method
+    let result: i32 = v.iter().sum();
+    println!("{}", result);
 
     // Creating a range iterator with `n..m`
     // Goes from n to m non-inclusive
     for i in 0..10 {
         println!("{}", i);
-    }
-
-    // Functions are allowed to be declared inside other functions
-    fn square(n: i32) -> i32 {
-        n * n
     }
 
     // The map method apply a function on every element of an iterator
@@ -38,4 +27,8 @@ fn main() {
     // You can create a function without defining its name by creating a closure
     (0..10).map(|n| n * n)
         .for_each(|n| println!("{}", n));
+}
+
+fn square(n: i32) -> i32 {
+    n * n
 }
