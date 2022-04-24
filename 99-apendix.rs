12b76403102d27b518a778a6649cf0a8bc155356
Cabidge <cabbageboi69@gmail.com>
2022-04-24 12:24:26 -0400

    tweak

--- 99-apendix.rs
+++ 99-apendix.rs
@@ -1,9 +1,6 @@
 // Want to try writing some rust for yourself?
 // Check out the Rust Playground! https://play.rust-lang.org/
 
-// Want to look up details about a specific feature?
-// Check out the rust docs! https://doc.rust-lang.org/std/index.html
-
 // Want to find some user-made libraries?
 // Check out crates.io! https://crates.io/
 
