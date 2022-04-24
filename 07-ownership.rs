c2404961f3c8dd8c63ded11af8a19a9f52f6b9c0
Cabidge <cabbageboi69@gmail.com>
2022-04-23 20:18:44 -0400

    tweak

--- 07-ownership.rs
+++ 07-ownership.rs
@@ -19,7 +19,6 @@ fn main() {
 
     // This doesn't work!
     //v.push(4);
-    //println!("{:?}", v);
 
     // Ownership of values can be kept by giving a clone instead of the original value
     let mut v = vec![1, 2, 3];
@@ -38,11 +37,6 @@ fn main() {
     // You can also give mutable references `&mut` to allow borrowers to update
     // the original value without giving up ownership
     change(&mut v);
-
-    // However, it is not possible to give a mutable reference to an immutable value
-    // This doesn't work!
-    //let v = Vec::new();
-    //change(&mut v);
 }
 
 // A function that takes ownership of a Vec
@@ -53,13 +47,10 @@ fn take(v: Vec<i32>) {
 // A function can borrow a value by taking a reference `&` to the value
 fn borrow(v: &Vec<i32>) {
     println!("I am borrowing the Vec: {:?}", v);
+} // The Vec does not get disposed because `borrow` does not own the value
 
-    // This doesn't work!
-    //v.push(4);
-} // The Vec does not get disposed of because `borrow` does not own the value
-
-// A function can borrow mutably byt taking a mutable reference `&mut`
+// A function can borrow mutably by taking a mutable reference `&mut`
 fn change(v: &mut Vec<i32>) {
     println!("I am updating the Vec: {:?}", v);
     v.push(4);
-} // The Vec does not get disposed of because `change` does not own the value
+} // The Vec does not get disposed because `change` does not own the value
