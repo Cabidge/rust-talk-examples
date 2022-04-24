fa006ead9f8bdfaf96817ad36da189122a95959b
Cabidge <cabbageboi69@gmail.com>
2022-04-23 23:15:16 -0400

    tweak

--- 09-structs.rs
+++ 09-structs.rs
@@ -1,20 +1,12 @@
 // `#[derive(Debug)]` macro allows formatting via {:?}
 #[derive(Debug)]
-// Create a struct with anonymous fields
-struct Point(i32, i32);
-
 // Create a struct with named fields
-#[derive(Debug)]
 struct Complex {
     real: f64,
     imag: f64,
 }
 
 fn main() {
-    // Instantiating tuple struct
-    let p = Point(2, 4);
-    println!("{:?}", p);
-
     // Instantiating named struct
     let i = Complex {
         real: 0.0,
