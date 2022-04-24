30e2ea8f509f42ce4f361b6f8b41195a638da332
Cabidge <cabbageboi69@gmail.com>
2022-04-23 20:09:22 -0400

    tweak

--- 05-arrays.rs
+++ 05-arrays.rs
@@ -1,8 +1,5 @@
 fn main() {
     // Array literals are declared with square brackets
-    // Arrays have a fixed size and are stored on the stack
-    // The type of an array is denoted with [T; n] for an array of
-    // some type T of size n
     let mut x = [1, 2, 3, 4];
 
     // Indexing an array (0-based index)
@@ -12,16 +9,12 @@ fn main() {
     x[1] = 3;
     println!("The second element of x is {}", x[1]);
 
-    // Replacing an entire array
-    x = [8, 8, 8, 8];
-    println!("The second element of x is {}", x[1]);
-
     // Create an array with repeating values
     let x = [3; 8];
 
     // Print the contents of an array
-    // Using `{:?}` formats the given value in "Debug" mode,
-    // which includes more information meant for the programmer
-    // Using `{}` is meant to be prettier and is for the end user
     println!("{:?}", x);
+
+    // `{}` is display mode for user-facing output
+    // `{:?}` is debug mode for programmer-facing output
 }
