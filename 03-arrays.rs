cad9b402b84145a75aecec1fa4be72dae140b6d3
Cabidge <cabbageboi69@gmail.com>
2022-04-24 16:14:13 -0400

    tweak

--- 03-arrays.rs
+++ 03-arrays.rs
@@ -1,13 +1,6 @@
 fn main() {
     // Array literals are declared with square brackets
-    let mut x = [1, 2, 3, 4];
-
-    // Indexing an array (0-based index)
-    println!("The second element of x is {}", x[1]);
-
-    // Mutating an array's elements
-    x[1] = 3;
-    println!("The second element of x is {}", x[1]);
+    let x = [1, 2, 3, 4];
 
     // Print the contents of an array
     println!("{:?}", x);
