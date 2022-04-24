3603d49af6ec6c79f4017aa90c1084960e41e2a9
Cabidge <cabbageboi69@gmail.com>
2022-04-24 10:30:33 -0400

    +pop

--- 09-option.rs
+++ 09-option.rs
@@ -5,4 +5,12 @@ fn main() {
 
     let maybe: Option<i32> = None; // None needs an explicit type
     println!("{:?}", maybe);
+
+    // Vec's pop method returns and removes the last element
+    // as an Option to represent a value that may or may not exist
+    let mut v = vec![1, 2];
+
+    println!("{:?}", v.pop());
+    println!("{:?}", v.pop());
+    println!("{:?}", v.pop());
 }
